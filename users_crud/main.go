e7c85c3c17efb9e70d6f848b2524900c1942302b
veylooirx. <veyloirxgx@gmail.com>
2026-08-15 20:14:27 -0700

    Creating CRUD functions

---
+++ users_crud/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt";
+	"errors";
+)
+
+type User struct {
+	ID int
+	Name string
+	Email  string
+}
+
+
+func main() {
+
+	users := []User{
+		{
+			ID: 12,
+			Name: "Israel L.",
+			Email: "johndoe@mail.com",
+		},
+		{
+			ID: 13,
+			Name: "Javier L.",
+			Email: "joanede@mail.com",
+		},
+		{
+			ID: 14,
+			Name: "Pako L.",
+			Email: "juande@mail.com",
+		},
+				{
+			ID: 15,
+			Name: "Pako L.",
+			Email: "juande@mail.com",
+		},
+				{
+			ID: 16,
+			Name: "Pako L.",
+			Email: "juande@mail.com",
+		},
+				{
+			ID: 17,
+			Name: "Pako L.",
+			Email: "juande@mail.com",
+		},
+	}
+
+}
+
+
+func createUser(users []User, NewUser User) []User {
+
+	users = append(users, NewUser)
+	return users
+}
+
+func updateUser(users []User, ID int, NewEmail string, NewName string) (*User, error){ 
+
+	for i := 0; i < len(users); i ++ {
+		if users[i].ID == ID {
+
+			userToUpdate := &users[i] 
+
+			userToUpdate.Email = NewEmail
+			userToUpdate.Name = NewName
+			
+			return userToUpdate, nil
+		}
+	}
+	return nil, errors.New("user not found")
+}
+
+func deleteUser(users []User, ID int) ([]User, error){
+
+	for i := 0; i < len(users); i ++ {
+		
+		if (users[i].ID == ID) {
+			users = append(users[:i], users[i+1:]...)
+			return users, nil
+		} 
+	}
+	return nil, errors.New("user not found")
+}
+
+func findUser(users []User, ID int) (*User, error){ 
+
+	for i := 0; i < len(users); i ++ {
+		if users[i].ID == ID {
+			return &users[i], nil
+		}
+	}
+	return nil, errors.New("user not found")
+}
