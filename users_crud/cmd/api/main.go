9f2ef383b783ebdeb4c974d1a32f1ad49669c930
veylooirx. <veyloirxgx@gmail.com>
2026-08-17 22:23:32 -0700

    Creating API struct

--- users_crud/cmd/api/main.go
+++ users_crud/cmd/api/main.go
@@ -1,100 +1,41 @@
 package main
 
 import (
-	"fmt";
-	"errors";
+    "fmt"
+    "users_crud/internal/user"
 )
-
-type User struct {
-	ID int
-	Name string
-	Email  string
-}
-
-var ErrUserNotFound = errors.New("user not found")
 
 func main() {
 
-	users := []User{
-		{
-			ID: 12,
-			Name: "Israel L.",
-			Email: "johndoe@mail.com",
-		},
-		{
-			ID: 13,
-			Name: "Javier L.",
-			Email: "joanede@mail.com",
-		},
-		{
-			ID: 14,
-			Name: "Pako L.",
-			Email: "juande@mail.com",
-		},
-				{
-			ID: 15,
-			Name: "Pako L.",
-			Email: "juande@mail.com",
-		},
-				{
-			ID: 16,
-			Name: "Pako L.",
-			Email: "juande@mail.com",
-		},
-				{
-			ID: 17,
-			Name: "Pako L.",
-			Email: "juande@mail.com",
-		},
-	}
+    users := []internal.User{
+        {
+            ID:    12,
+            Name:  "Israel L.",
+            Email: "johndoe@mail.com",
+        },
+        {
+            ID:    13,
+            Name:  "Javier L.",
+            Email: "joanede@mail.com",
+        },
+        {
+            ID:    14,
+            Name:  "Pako L.",
+            Email: "juande@mail.com",
+        },
+    }
 
-	result, err := updateUser(users, 13, "pakomail", "johndoe")
+    result, err := internal.UpdateUser(
+        users,
+        13,
+        "pakomail",
+        "johndoe",
+    )
 
-	if err != nil {
-		return
-	}
-	fmt.Println(*result)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
 
-}
-
-func createUser(users []User, NewUser User) []User {
-
-	users = append(users, NewUser)
-	return users
-}
-
-func updateUser(users []User, ID int, NewEmail string, NewName string) (*User, error){ 
-
-	userToUpdate, err := findUser(users, ID)
-
-	if err != nil {
-		return nil, err
-	}
-
-	userToUpdate.Email = NewEmail
-	userToUpdate.Name = NewName
-
-	return userToUpdate, nil
-}
-
-func deleteUser(users []User, ID int) ([]User, error){
-
-	for i := range users {
-		if users[i].ID == ID {
-			users =  append(users[:i], users[i+1:]...)
-			return users, nil 
-		}
-	}
-	return users, ErrUserNotFound
-}
-
-func findUser(users []User, ID int) (*User, error){ 
-
-	for i := range users{
-
-		if users[i].ID == ID {
-			return &users[i], nil
-		}
-	}
-	return nil, ErrUserNotFound
+    fmt.Println(*result)
 }
